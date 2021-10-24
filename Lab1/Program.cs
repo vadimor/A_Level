cfc31dd51d9acb7778c91ecfbbfc3ffb25b71891
vadimor <vadimorbiluy@gmail.com>
2021-10-24 19:15:41 +0300

    FinalTest

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -6,7 +6,7 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-          Console.WriteLine("Hello World!1111");
+          Console.WriteLine("FinalTest");
         }
     }
 }
